37c89044af8e593d091732761d9bf76bd4c599bb
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-10-08 12:03:30 -0400

    Added Product model

--- apps/store/models.py
+++ apps/store/models.py
@@ -5,3 +5,23 @@ class Store(models.Model):
 
     def __str__(self):
         return self.name 
+
+class Product(models.Model):
+    name = models.CharField(max_length=255)
+    description = models.TextField()
+    price = models.DecimalField(max_digits=6, decimal_places=2)
+    store = models.ForeignKey(Store, on_delete=models.CASCADE)
+    pub_date = models.DateTimeField(auto_now_add=True)
+    last_edit = models.DateTimeField(auto_now=True)
+    handle = models.SlugField(blank=True, null=True)
+
+    def save(self, *args, **kwargs):
+        if self.handle is None:
+            self.handle = slugify(self.name)
+        else:
+            self.handle = slugify(self.handle)
+
+        super(Product, self).save(*args, **kwargs)
+
+    def __str__(self):
+        return self.name
