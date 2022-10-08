640686c060b3ed329f82ba62686c8aee03afbd0d
Nawaz Gafar <gafar.nawaz@gmail.com>
2022-10-08 15:10:11 -0400

    Added placeholder views for store app

--- apps/store/urls.py
+++ apps/store/urls.py
@@ -2,6 +2,23 @@ from django.urls import path
 from . import views, api
 
 urlpatterns = [
+    # views.py
+    path('store/create/', views.create_store),
+    path('store/read/<int:store_id>/', views.read_store),
+    path('store/update/<int:store_id>/', views.update_store),
+    path('store/delete/<int:store_id>/', views.delete_store),
+    
+    path('product/create/', views.create_product),
+    path('product/read/<int:product_id>/', views.read_product),
+    path('product/update/<int:product_id>/', views.update_product),
+    path('product/delete/<int:product_id>/', views.delete_product),
+
+    path('variant/create/', views.create_variant),
+    path('variant/read/<int:variant_id>/', views.read_variant),
+    path('variant/update/<int:variant_id>/', views.update_variant),
+    path('variant/delete/<int:variant_id>/', views.delete_variant),
+
+    # api.py
     path('api/all-products/', api.all_products),
     path('api/product/<int:product_id>/', api.specific_product),
     path('api/all-variants/<int:product_id>/', api.all_variants),
--- apps/store/views.py
+++ apps/store/views.py
@@ -1,3 +1,52 @@
 from django.shortcuts import render
+from .models import Store, Product, Variant 
 
-# Create your views here.
+#----------------
+# STORE
+#----------------
+
+def create_store(request):
+    pass 
+
+def read_store(request, store_id):
+    pass 
+
+def update_store(request, store_id):
+    pass
+
+def delete_store(request, store_id):
+    pass 
+
+
+#----------------
+# PRODUCT
+#----------------
+
+def create_product(request):
+    pass
+
+def read_product(request, product_id):
+    pass 
+
+def update_product(request, product_id):
+    pass
+
+def delete_product(request, product_id):
+    pass 
+
+
+#----------------
+# VARIANT
+#----------------
+
+def create_variant(request):
+    pass
+
+def read_variant(request, variant_id):
+    pass 
+
+def update_variant(request, variant_id):
+    pass
+
+def delete_variant(request, variant_id):
+    pass 
